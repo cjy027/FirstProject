ce414be95de840775faa7f28312d9b438c07a435
cjy <563482721@qq.com>
2018-03-25 17:30:46 +0800

    dsadasdas

---
+++ 01-刷帧/01-刷帧/NJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NJAppDelegate.h
+//  01-刷帧
+//
+//  Created by apple on 14-6-10.
+//  Copyright (c) 2014年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 01-刷帧/01-刷帧/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  01-刷帧
+//
+//  Created by apple on 14-6-10.
+//  Copyright (c) 2014年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NJAppDelegate class]));
+    }
+}
